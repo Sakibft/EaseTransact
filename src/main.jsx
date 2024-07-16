802079e69a8afb17ebecea1539703881c4e8b984
Sahadat Hosen <ssar96540@gmail.com>
2024-07-16 17:41:09 +0600

    basick project setupd done

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+ 
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <div className='text-red-600 font-bold text-4xl text-center'>hello world</div>
+   <div><button className='btn btn-primary'>hahah</button></div>
+  </React.StrictMode>,
+)
